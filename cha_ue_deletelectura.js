daa501eb6f65070aafd6f5b56644eda17beeac37
Enrique Martin <enrique.martin@chapur.com>
2024-12-10 12:34:50 -0600

    beforeSubmit de referencia. deletelectura

--- cha_ue_deletelectura.js
+++ cha_ue_deletelectura.js
@@ -12,15 +12,62 @@ define(
     const entry_point = {
       beforeSubmit: (context) => {}
     };
-
+    
     entry_point.beforeSubmit = (context) => {
-      log.debug("log context before", context.type);
-    }
+      //log.debug("log context before", context.type);
+      if(context.type === "edit"|| context.type === "delete"){
+        log.debug("context", context);
+      }
 
-    entry_point.afterSubmit = (context) => {
-      log.debug("log context after", context.type);
     }
     
     return entry_point;
 
   });
+
+
+  function beforeSubmit(context) {
+    //saveLog("BeforeSubmit context", context)
+    //log.debug("Beforesubtmit TYPE_VIEW_SUBMIT", context.type);
+    if (context.type == context.UserEventType.DELETE) {
+        if (context.newRecord.getValue({
+                fieldId: 'type'
+            })) {
+            var vendorPayment = context.newRecord;
+        } else {
+            var vendorPayment = context.oldRecord;
+        }
+
+        //log.debug("vendor DAta", vendorPayment);
+        //log.debug("subsidiary", vendorPayment.getValue({ fieldId: 'subsidiary' }));
+
+        //Validacion pra cuando llegue el evento desde microsip
+        if (!(vendorPayment.getValue({
+                fieldId: 'subsidiary'
+            }) && vendorPayment.getLineCount({
+                sublistId: 'apply'
+            }))) {
+            log.debug("NO llegaron los campos subsidiary y apply");
+            recordID = vendorPayment.id;
+            try {
+                vendorPayment = record.load({
+                    type: record.Type.CUSTOMER_PAYMENT,
+                    id: recordID,
+                    isDynamic: true
+                });
+            } catch (error) {
+
+            }
+        }
+        var numRows = vendorPayment.getLineCount({
+                sublistId: 'apply'
+            }),
+            reConfig = customDataAPI.getConfig(vendorPayment.getValue({
+                fieldId: 'subsidiary'
+            })),
+            billIdList = [];
+
+        if (reConfig) {
+        }
+    }
+}
